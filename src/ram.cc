1d16af36a5b14f321e879b0290c931042de98c00
Dario Dominguez Gonzalez <alu0101408095@ull.edu.es>
2025-02-11 09:20:09 +0000

    Primera prueba

--- src/ram.cc
+++ src/ram.cc
@@ -10,6 +10,9 @@
  *          
 */
 
+#include <iostream>
+#include <fstream>
+#include <string>
 
 #include "Clases/aluUnit.h"
 #include "Clases/dataMemory.h"
@@ -17,6 +20,35 @@
 #include "Clases/outputUnit.h"
 #include "Clases/programMemory.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
+		std::cerr << "Error: Numero de argumentos incorrecto." << std::endl;
+		std::cerr << "Uso: ./ram  <archivoEntrada> <archivoSalida>" << std::endl;
+		return 1;
+	}
+
+	std::string input_file = "../CintaEntrada/";
+	std::string output_file = "../CintaSalida/";
+	input_file += argv[1];
+	output_file += argv[2];
+	std::ifstream input_stream(input_file);
+	std::ofstream output_stream(output_file);
+
+	if (!input_stream.is_open()) {
+		std::cerr << "Error: No se pudo abrir el archivo de entrada." << std::endl;
+		return 1;
+	}
+	if (!output_stream.is_open()) {
+		std::cerr << "Error: No se pudo abrir el archivo de salida." << std::endl;
+		return 1;
+	}
+
+	while (!input_stream.eof()) {
+		std::string line;
+		std::getline(input_stream, line);
+		std::cout << line << std::endl;
+		output_stream << line << std::endl;
+	}
+
   return 0;
 }
